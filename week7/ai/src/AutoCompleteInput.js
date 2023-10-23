322030752a5b06c89e5c27adffad697972665562
Echomo Xinyu <seanxinyu2@gmail.com>
2023-10-23 16:59:09 -0700

    fix error

--- week7/ai/src/AutoCompleteInput.js
+++ week7/ai/src/AutoCompleteInput.js
@@ -4,7 +4,7 @@ export default function AutoCompleteInput(props) {
        <input type="text" list={props.id} value={props.value} onChange={(event) => {
         props.selfOnChange(event);
       }}/>
-      <datalist id={id}>
+      <datalist id={props.id}>
         {props.options.map((option) => {
           return <option key={option} value={option}/>
         })}
